f3a9d790661373696714546a4742e334ac404f36
逍遥 <2447315136@qq.com>
2025-10-15 20:56:57 +0800

    初始化项目

---
+++ src/test/java/com/xy/aicodegenerator/XyAiCodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xy.aicodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyAiCodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
