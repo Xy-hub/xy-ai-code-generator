b282c1a2dd4d030ee0602635883cb630226e887d
逍遥 <2447315136@qq.com>
2025-11-19 20:49:25 +0800

    LangGraph4j 基础框架构建完成，实现假数据全流程通过

---
+++ src/main/java/com/xy/aicodegenerator/langgraph/node/ImageCollectorNode.java
@@ -0,0 +1,43 @@
+package com.xy.aicodegenerator.langgraph.node;
+
+import com.xy.aicodegenerator.langgraph.model.ImageResource;
+import com.xy.aicodegenerator.langgraph.model.enums.ImageCategoryEnum;
+import com.xy.aicodegenerator.langgraph.state.WorkflowContext;
+import lombok.extern.slf4j.Slf4j;
+import org.bsc.langgraph4j.action.AsyncNodeAction;
+import org.bsc.langgraph4j.prebuilt.MessagesState;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class ImageCollectorNode {
+    public static AsyncNodeAction<MessagesState<String>> create() {
+        return AsyncNodeAction.node_async(state -> {
+            WorkflowContext context = WorkflowContext.getContext(state);
+            log.info("执行节点: 图片收集");
+            
+            // TODO: 实际执行图片收集逻辑
+            
+            // 简单的假数据
+            List<ImageResource> imageList = Arrays.asList(
+                ImageResource.builder()
+                    .category(ImageCategoryEnum.CONTENT)
+                    .description("假数据图片1")
+                    .url("https://www.codefather.cn/logo.png")
+                    .build(),
+                ImageResource.builder()
+                    .category(ImageCategoryEnum.LOGO)
+                    .description("假数据图片2")
+                    .url("https://www.codefather.cn/logo.png")
+                    .build()
+            );
+            
+            // 更新状态
+            context.setCurrentStep("图片收集");
+            context.setImageList(imageList);
+            log.info("图片收集完成，共收集 {} 张图片", imageList.size());
+            return WorkflowContext.saveContext(context);
+        });
+    }
+}
---
+++ src/main/java/com/xy/aicodegenerator/langgraph/node/RouterNode.java
@@ -0,0 +1,27 @@
+package com.xy.aicodegenerator.langgraph.node;
+
+import com.xy.aicodegenerator.langgraph.state.WorkflowContext;
+import com.xy.aicodegenerator.model.enums.CodeGenTypeEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.bsc.langgraph4j.action.AsyncNodeAction;
+import org.bsc.langgraph4j.prebuilt.MessagesState;
+
+@Slf4j
+public class RouterNode {
+    public static AsyncNodeAction<MessagesState<String>> create() {
+        return AsyncNodeAction.node_async(state -> {
+            WorkflowContext context = WorkflowContext.getContext(state);
+            log.info("执行节点: 智能路由");
+            
+            // TODO: 实际执行智能路由逻辑
+            
+            // 简单的假数据
+            CodeGenTypeEnum generationType = CodeGenTypeEnum.HTML;
+            // 更新状态
+            context.setCurrentStep("智能路由");
+            context.setGenerationType(generationType);
+            log.info("路由决策完成，选择类型: {}", generationType.getText());
+            return WorkflowContext.saveContext(context);
+        });
+    }
+}
