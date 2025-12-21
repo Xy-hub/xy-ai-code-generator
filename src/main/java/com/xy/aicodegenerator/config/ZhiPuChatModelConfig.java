49f7552dbd4e05fe9949192ed4aca7c8aa7709c0
逍遥 <2447315136@qq.com>
2025-12-21 14:54:52 +0800

    不引入智谱官方依赖，直接使用openai的依赖，更换baseurl及api key即可使用glm模型

--- src/main/java/com/xy/aicodegenerator/config/ZhiPuChatModelConfig.java
+++ src/main/java/com/xy/aicodegenerator/config/ZhiPuChatModelConfig.java
@@ -1,7 +1,7 @@
 package com.xy.aicodegenerator.config;
 
-import dev.langchain4j.community.model.zhipu.ZhipuAiChatModel;
-import dev.langchain4j.community.model.zhipu.ZhipuAiStreamingChatModel;
+//import dev.langchain4j.community.model.zhipu.ZhipuAiChatModel;
+//import dev.langchain4j.community.model.zhipu.ZhipuAiStreamingChatModel;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
 //import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
@@ -26,25 +26,25 @@ public class ZhiPuChatModelConfig {
     /**
      * 推理流式模型（用于 Vue 项目生成，带工具调用）
      */
-    @Bean
-    public StreamingChatModel zhiPuStreamingChatModel() {
-         return ZhipuAiStreamingChatModel.builder()
-                .apiKey(apiKey)
-                .model(modelName)
-                .maxToken(maxTokens)
-                .logRequests(true)
-                .logResponses(true)
-                .build();
-    }
-
-    @Bean
-    public ChatModel zhiPuChatModel() {
-        return ZhipuAiChatModel.builder()
-                .apiKey(apiKey)
-                .model(modelName)
-                .maxToken(maxTokens)
-                .logRequests(true)
-                .logResponses(true)
-                .build();
-    }
+//    @Bean
+//    public StreamingChatModel zhiPuStreamingChatModel() {
+//         return ZhipuAiStreamingChatModel.builder()
+//                .apiKey(apiKey)
+//                .model(modelName)
+//                .maxToken(maxTokens)
+//                .logRequests(true)
+//                .logResponses(true)
+//                .build();
+//    }
+//
+//    @Bean
+//    public ChatModel zhiPuChatModel() {
+//        return ZhipuAiChatModel.builder()
+//                .apiKey(apiKey)
+//                .model(modelName)
+//                .maxToken(maxTokens)
+//                .logRequests(true)
+//                .logResponses(true)
+//                .build();
+//    }
 }
